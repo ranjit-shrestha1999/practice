a5919e89fcba5959d28bcbc9bfa3ba3477af4f8a
ranjit-shrestha1999 <ranjitstha1999>
2022-06-03 08:34:29 +0545

    newchanges

--- lib/main.dart
+++ lib/main.dart
@@ -8,24 +8,71 @@ class ScoreApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Score App",
       home: ScoreHome(),
     );
   }
 }
 
-class ScopeHome extends StatefulWidget {
+class ScoreHome extends StatefulWidget {
+  const ScoreHome({Key? key}) : super(key: key);
+
   @override
-  State<ScopeHome> createState() => _ScopeHomeState();
+  State<ScoreHome> createState() => _ScoreHomeState();
 }
 
-class _ScopeHomeState extends State<ScopeHome> {
+class _ScoreHomeState extends State<ScoreHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Score App"),
+        title: Text("Score Changer"),
         centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 25,
+          ),
+          Center(
+            child: Text(
+              "Score is",
+              style: TextStyle(
+                color: Colors.green,
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          Center(
+            child: Text(
+              "0",
+              style: TextStyle(
+                color: Colors.green,
+                fontSize: 90,
+                fontWeight: FontWeight.w800,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue,
+                  
+                ),
+                child: Text("Increase"),\
+                onPressed: (){},
+
+              )
+            ],
+          )
+        ],
       ),
     );
   }
